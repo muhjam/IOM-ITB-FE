3bfd2a465c6567f18d3481e8e60b783e2d9e1664
Muhamad Jamaludin <muhhjam@gmail.com>
2024-07-14 15:13:21 +0700

    update

--- src/views/pages/AboutView.vue
+++ src/views/pages/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white pt-[80px]">
+  <div class="bg-white pt-[80px] pb-[32px]">
     <h2 class="text-main px-[30px] md:px-[70px] font-[800] text-[32px] md:text-[50px] leading-tight md:leading-[65.1px] py-[16px]">Latar Belakang</h2>
       <div class="w-full px-[30px] md:px-[70px]">
         <h4 class="text-[24px] md:text-[26px] capitalize text-main font-[700]">Sejak 1974 IOM-ITB Telah Banyak Membantu Mahasiswa yang Kesulitan Finansial</h4>
@@ -7,29 +7,6 @@
         {{ about }}
         </p>
       </div>
-  </div>
-
-  <div class="bg-white p-[16px] md:p-[24px]">
-    <h2 class="text-main font-[800] text-[32px] md:text-[50px] text-center leading-tight md:leading-[65.1px] py-[16px]">Kegiatan</h2>
-    <div class="flex overflow-x-auto gap-4 snap-x">
-      <div v-for="(v, i) in activity" :key="i" class="min-w-[250px] md:min-w-[300px] bg-main text-white border border-gray-200 rounded-lg shadow snap-center">
-        <a href="#">
-          <img class="rounded-t-lg" :src="require(`@/assets/image/${v.image}`)" alt="" />
-        </a>
-        <div class="p-5">
-          <a href="#">
-            <h5 class="mb-2 text-xl md:text-2xl font-bold tracking-tight text-white">{{ v.title }}</h5>
-          </a>
-          <p class="mb-3 font-normal text-white">{{ v.description }}</p>
-          <a :href="v.url" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-main bg-white rounded-full hover:opacity-[0.8] focus:ring-4 focus:outline-none focus:ring-blue-300">
-            Baca Selengkapnya
-            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
   </div>
 
   <div class="bg-colorSecond py-[50px] md:py-[104px] px-[20px] md:px-[42px] text-center">
