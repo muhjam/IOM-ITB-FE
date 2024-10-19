69b3d591d6bfdbe4a5ae7765464d0ee9d4173d23
Muhamad Jamaludin <muhhjam@gmail.com>
2024-10-19 09:57:04 +0700

    update store

--- src/views/pages/StoreView.vue
+++ src/views/pages/StoreView.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="bg-colorSecond pt-[100px] pb-[32px] px-[18px] md:px-[70px]">
+  <div class="bg-colorSecond pt-[100px] pb-[32px] px-[18px] md:px-[70px] flex justify-center">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <div
         v-for="(merchandise, idx) in merchandises?.data"
         :key="merchandise.id"
         class="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
       >
-        <a href="#">
+        <div class="w-full h-[300px] overflow-hidden">
           <img
             v-if="merchandise.image"
             :src="merchandise.image"
             :alt="merchandise.name"
-            class="rounded-t-lg"
+            class="rounded-t-lg object-cover"
           />
           <img
             v-else
@@ -19,8 +19,8 @@
             alt="No Image"
             class="rounded-t-lg"
           />
-        </a>
-        <div class="p-5">
+        </div>
+        <div class="p-5 bg-white">
           <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
               {{ merchandise.name }}
@@ -33,14 +33,14 @@
             {{ formatPrice(merchandise.price) }}
           </p>
           <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
-            Stock: {{ merchandise.stock }}
+            Stok: {{ merchandise.stock }}
           </p>
           <div v-if="merchandise.stock > 0">
             <a
               :href="`store/${merchandise.id}`"
               class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
             >
-              Buy Now
+              Beli Sekarang
               <svg
                 class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
