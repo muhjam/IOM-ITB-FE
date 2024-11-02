4a65832fe857e78e6f8d855e6e927a01584db650
Muhamad Jamaludin <muhhjam@gmail.com>
2024-11-02 19:40:16 +0700

    update grafik

--- src/components/item/GraphicItem.vue
+++ src/components/item/GraphicItem.vue
@@ -3,19 +3,14 @@
       <h2 class="text-main font-[800] text-[32px] md:text-[50px] text-center leading-tight md:leading-[65.1px] py-[16px]">Grafik</h2>
       <div class="flex flex-col md:flex-row justify-between md:items-center mb-[8px] gap-2 md:px-10">
         <div class="flex gap-2 items-center">
-         <button @click="modeHandler('year')" :class="`${mode === 'year' ? 'text-white bg-main' : 'text-main bg-white'} border border-main px-[8px] py-[4px] w-fit h-fit rounded-[5px] text-[14px] md:text-[16px]`">Pertahun</button> <button @click="modeHandler('perbulan')" :class="`${mode === 'perbulan' ? 'text-white bg-main' : 'text-main bg-white'} border border-main px-[8px] py-[4px] w-fit h-fit rounded-[5px] text-[14px] md:text-[16px]`">Perbulan</button> <button @click="modeHandler('grade')" :class="`${mode === 'grade' ? 'text-white bg-main' : 'text-main bg-white'} border border-main px-[8px] py-[4px] w-fit h-fit rounded-[5px] text-[14px] md:text-[16px]`">Perangkatan</button>
+         <button @click="modeHandler('year')" :class="`${mode === 'year' ? 'text-white bg-main' : 'text-main bg-white'} border border-main px-[8px] py-[4px] w-fit h-fit rounded-[5px] text-[14px] md:text-[16px]`">Pertahun</button> <button @click="modeHandler('grade')" :class="`${mode === 'grade' ? 'text-white bg-main' : 'text-main bg-white'} border border-main px-[8px] py-[4px] w-fit h-fit rounded-[5px] text-[14px] md:text-[16px]`">Perangkatan</button>
         </div>
-         <div v-if="mode !== 'perbulan'" class="w-full md:w-fit h-fit flex">
+         <div class="w-full md:w-fit h-fit flex">
               <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-[14px] md:text-[16px] rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full px-[8px] py-[4px]" v-model="graphic">
                 <option selected value="0">Rekap Semua</option>
                 <option value="1">Biaya Hidup</option>
                 <option value="2">Tugas Akhir</option>
                 <option value="3">UKT</option>
-              </select>
-         </div>
-         <div v-else class="w-full md:w-fit h-fit flex">
-              <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-[14px] md:text-[16px] rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full px-[8px] py-[4px]" v-model="graphic">
-                <option selected value="0">Rekap Semua</option>
               </select>
          </div>
       </div>
@@ -35,9 +30,6 @@
           require('@/assets/image/grafik-biaya-hidup-pertahun.png'),
           require('@/assets/image/grafik-tugas-akhir-pertahun.png'),
           require('@/assets/image/grafik-ukt-pertahun.png')
-        ]},
-        {category: 'month', graphic: [
-          require('@/assets/image/grafik-kegiatan-perbulan.png'),
         ]},
         {category: 'grade', graphic: [
           require('@/assets/image/grafik-rekap-perangkatan.png'),
