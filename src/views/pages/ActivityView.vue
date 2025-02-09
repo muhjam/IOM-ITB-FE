9db1a124e3acca0b7c024daa1d8e7e2656fc1c57
muhjam <muhhjam@gmail.com>
2025-02-10 01:10:20 +0700

    update

--- src/views/pages/ActivityView.vue
+++ src/views/pages/ActivityView.vue
@@ -52,7 +52,7 @@ export default {
   },
   computed:{
     activities(){
-      return this.$store.getters.activities?.data;
+      return this.$store.getters.activities;
     },
   },
   async mounted(){
