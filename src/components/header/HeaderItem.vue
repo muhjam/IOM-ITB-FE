6ee70f0608da2610195560f0abdfda3d486db85d
muhjam <muhhjam@gmail.com>
2025-03-19 14:53:14 +0700

    update

--- src/components/header/HeaderItem.vue
+++ src/components/header/HeaderItem.vue
@@ -1,38 +1,43 @@
 <template>
 	<Carousel>
-	  <Slide v-for="slide in 4" :key="slide">
-		<div v-if="slide === 1" class="relative h-[30vh] sm:h-[90vh] w-full bg-cover bg-no-repeat">
+	  <Slide v-for="slide in items" :key="slide">
+		<div v-if="slide === 'slide1.mp4'" class="relative h-[30vh] sm:h-[90vh] w-full bg-cover bg-no-repeat">
 		  <video class="absolute inset-0 w-full h-full object-cover" autoplay muted playsinline>
 			<source src="@/assets/video/slide1.mp4" type="video/mp4">
 			Your browser does not support the video tag.
 		  </video>
 		</div>
-		<div v-else class="relative h-[30vh] sm:h-[90vh] w-full bg-cover bg-no-repeat" :style="{ backgroundImage: `url(${require(`@/assets/image/slide${slide}.png`)})` }"></div>
+		<div v-else class="relative h-[30vh] sm:h-[90vh] w-full bg-cover bg-no-repeat" :style="{ backgroundImage: `url(${require(`@/assets/image/${slide}`)})` }"></div>
 	  </Slide>
   
 	  <template #addons>
 		<Navigation />
 	  </template>
 	</Carousel>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue'
-  import { Carousel, Navigation, Slide } from 'vue3-carousel'
-  
-  import 'vue3-carousel/dist/carousel.css'
-  
-  export default defineComponent({
-	name: 'Basic',
-	components: {
-	  Carousel,
-	  Slide,
-	  Navigation,
-	},
-  })
-  </script>
-  
-  <style>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Slide } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
+export default defineComponent({
+  name: 'Basic',
+  data() {
+    return {
+      items: ["slide1.mp4", "slide2.png", "slide3.png", "slide4.png", "slide5.png"]
+    };
+  },
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
+})
+</script>
+
+<style>
   .carousel__item {
 	min-height: 200px;
 	width: 100%;
@@ -52,4 +57,3 @@
 	border: none; /* Optional: Remove border if any */
   }
   </style>
-  
